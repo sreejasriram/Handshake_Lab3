bc3a0104742fec6eb8abe63cbf0ce219d5b80300
sreejasriram <sreeja.sriram@sjsu.edu>
2020-10-15 03:37:33 -0700

    mongo url removed

--- Handshake/backend/Database/config.js
+++ Handshake/backend/Database/config.js
@@ -1,5 +1,5 @@
 const config = {
-    mongoDB: 'mongodb+srv://root:5151@handshake-cluster-crb7q.mongodb.net/handshake_lab3_test?retryWrites=true&w=majority'
+    mongoDB: ''
 };
 
 module.exports = config;
